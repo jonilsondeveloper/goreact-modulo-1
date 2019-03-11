713966cd18a1508a7d98f7d65e6788417be0a3b0
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-03-11 20:01:37 -0300

    criando primeiro componente

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+
+class Button extends Component {
+  render() {
+    return <a href="">Enviar</a>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
